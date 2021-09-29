29a5f1f0f74ccafa338b24cdcd25e7b22a2689ce
maedahiroshi <aisys0210@gmail.com>
2021-09-29 16:28:32 +0900

    ユーザ認証できるようにした

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,21 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-  #   include SessionHelper
+    include SessionsHelper
     
-  #   identified_by :message_user
+    identified_by :message_user
     
-  #   def connect
-  #     self.message_user = find_verified_user
-  #   end
+    def connect
+      self.message_user = find_verified_user
+    end
     
-  #   private
+    private
     
-  #     def find_verified_user
-  #       if logged_in?
-  #         current_user
-  #       else
-  #         reject_unauthorized_connection
-  #       end
-  #     end
+      def find_verified_user
+        if logged_in?
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
